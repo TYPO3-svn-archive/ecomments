8e9628b263241f260212018e1c936c5caa334a36
maikh <maikh@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-20 16:17:56 +0000

    initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ecomments/trunk@83307 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Ecomments',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => '',
+	'author_email' => '',
+	'author_company' => '',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.1',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '1.3',
+			'fluid' => '1.3',
+			'typo3' => '6.2.0-6.2.x',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
